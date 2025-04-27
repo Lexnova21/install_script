57893e72e0838ff9c373e20be1c7927e3bf0b8c5
Lexnova21 <alexander.tomek@gmail.com>
2025-04-27 21:02:54 +0200

    fixes

--- modules/packages.sh
+++ modules/packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 install_base_packages() {
-  local common_packages="hyprland udisks2 sudo python-pip swaync waybar hypridle wlroots networkmanager xdg-desktop-portal-hyprland hyprland-qt-support hyprpolkitagent thunar gvfs wofi vim kitty nwg-look gnome-themes-extra materia-gtk-theme power-profiles-daemon "
+  local common_packages="hyprland udisks2 sudo thunderbird python-pip swaync waybar hypridle wlroots networkmanager xdg-desktop-portal-hyprland hyprland-qt-support hyprpolkitagent thunar gvfs wofi vim kitty nwg-look gnome-themes-extra materia-gtk-theme power-profiles-daemon "
   local arch_only_packages="wayland"
 
   if [[ "$os" == "arch" ]]; then
